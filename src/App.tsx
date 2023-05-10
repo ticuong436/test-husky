a2e32a2905abbff35fbe5039a888c059d0d3467a
cuong <gsoftda3full2@gmail.com>
2023-05-10 13:35:44 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsssss ss
+          Learn Reactsssss dsda
         </a>
       </header>
     </div>
